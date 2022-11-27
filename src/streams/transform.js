83a1ac131ec9eb7b267bf6850a04088f3db9fc66
py-cs <e.1@mail.ru>
2022-11-27 23:00:19 +0300

    feat: add streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { stdin, stdout } from "process";
+import { Transform } from "stream";
+import { EOL } from "os";
+
 const transform = async () => {
-    // Write your code here 
+  const reverse = new Transform({
+    transform(chunk, _, callback) {
+      callback(
+        null,
+        String(chunk).replace(EOL, "").split("").reverse().join("") + EOL
+      );
+    },
+  });
+  stdin.pipe(reverse).pipe(stdout);
 };
 
 await transform();
