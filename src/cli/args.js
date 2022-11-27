c37265becd943b428f1da80779a9b1a75e76c49e
py-cs <e.1@mail.ru>
2022-11-27 22:23:41 +0300

    feat: add cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const argsStr = args
+    .reduce((acc, arg, index, arr) => {
+      if (arg.startsWith("--")) {
+        acc.push(`${arg.slice(2)} is ${arr[index + 1]}`);
+      }
+      return acc;
+    }, [])
+    .join(", ");
+
+  console.log(argsStr);
 };
 
 parseArgs();
