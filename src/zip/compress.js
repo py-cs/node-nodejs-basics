f99cdcda4c286e4a66539e42d669832bac5c487f
py-cs <e.1@mail.ru>
2022-11-27 23:03:08 +0300

    feat: add streams/transform

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import path from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import { createGzip } from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const srcFile = path.resolve(__dirname, "files", "fileToCompress.txt");
+  const destFile = path.resolve(__dirname, "files", "archive.gz");
+
+  const read = createReadStream(srcFile);
+  const write = createWriteStream(destFile);
+  const zip = createGzip();
+
+  read.pipe(zip).pipe(write);
 };
 
 await compress();
