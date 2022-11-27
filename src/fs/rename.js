9efe1398215ced7e3ee600010e124f5bd4242eb9
py-cs <e.1@mail.ru>
2022-11-27 22:14:58 +0300

    feat: add fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import path from "node:path";
+import { rename as renameFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+
 const rename = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const wrongFilename = path.resolve(__dirname, "files", "wrongFilename.txt");
+  const properFilename = path.resolve(__dirname, "files", "properFilename.md");
+  const errorMessage = "FS operation failed";
+
+  renameFile(wrongFilename, properFilename).catch(() => {
+    throw new Error(errorMessage);
+  });
 };
 
 await rename();
